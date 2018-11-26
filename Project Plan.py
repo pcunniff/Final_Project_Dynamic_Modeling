babefe40918731622747346c26bb6ad984670ebe
pcunniff <43554982+pcunniff@users.noreply.github.com>
2018-11-26 10:53:55 -0500

    Cole, Zach, and Patrick's plan for the project

---
+++ Project Plan.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 26 10:47:09 2018
+
+@author: Patrick
+"""
+
+#Zach Bonstetter, Patrick Cunniff and Cole Grabowski will be working together 
+#on the Python Dynamic Modelling project for Intro to Biocomputing:
+
+#Steps to completing project:
+
+#1) Zach will be responsible for drawing the conceptual models
+#for both Lotka -Volterra and Rosenzweig-MacArthur models
+
+#2) Patrick will then be responsible for completing the remaining
+#steps for the Lotka-Volterra model.
+
+#3) Cole will be responsible for then completing the remaining steps for
+#the Rosenzweig-MacArthur model.
+
+#4) Based on Cole's completion of the steps for the Rosenzweig-MacArthur model
+#Zach will be responsible for completing the steps listed under the
+#Paradox of Enrichment.
+
