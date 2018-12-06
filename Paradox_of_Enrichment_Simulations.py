bb507412824e482b6a7a9cbb025325b00f6310d9
root <root@DESKTOP-0Q2D8CL.localdomain>
2018-12-06 13:16:50 -0500

    updated Paradox of Enrichment Simulations Code

--- Paradox_of_Enrichment_Simulations.py
+++ Paradox_of_Enrichment_Simulations.py
@@ -19,6 +19,8 @@ y0=[500,120] # passed to RMA_Sim as y in this case, you can call it y0=y0 since 
 
 # b represents prey birth rate
 b = 0.8
+# a represents alpha (1 / carrying capacity)
+a = 0.00125
 # w represents search rate (units of area per time)
 w = 5
 # d represents density of prey at which the predators kill rate reaches half its maximum
@@ -29,28 +31,28 @@ e = 0.07
 s = 0.2
 
 # 1st simulation, carrying capacity = 800, alpha = 0.00125
-params = (b, 0.00125, w, d, e, s) # params given in the order provided above, aside from the alpha value that varies every trial
+params = (b, a, w, d, e, s) # params given in the order provided above, aside from the alpha value that varies every trial
 sim = spint.odeint(func = RMA_Sim, y0 = y0, t = times, args = params)
 simDF = pandas.DataFrame({"Time":times,"Population":sim[:,0],"Population2":sim[:,1]})
 a1 = ggplot(simDF,aes(x="Time",y="Population"))+geom_line()+geom_line(simDF,aes(x="Time",y="Population2"),color='red')+theme_classic()
 print(a1)
 
 # 2nd simulation, carrying capacity = 1200, alpha = 0.001
-params = (b, 0.001, w, d, e, s)
+params = (b, a*0.8, w, d, e, s)
 sim = spint.odeint(func = RMA_Sim, y0 = y0, t = times, args = params)
 simDF = pandas.DataFrame({"Time":times,"Population":sim[:,0],"Population2":sim[:,1]})
 a2 = ggplot(simDF,aes(x="Time",y="Population"))+geom_line()+geom_line(simDF,aes(x="Time",y="Population2"),color='red')+theme_classic()
 print(a2)
 
 # 3rd simulation, carrying capacity = 1600, alpha = 0.00075
-params = (b, 0.00075, w, d, e, s)
+params = (b, a*0.6, w, d, e, s)
 sim = spint.odeint(func = RMA_Sim, y0 = y0, t = times, args = params)
 simDF = pandas.DataFrame({"Time":times,"Population":sim[:,0],"Population2":sim[:,1]})
 a3 = ggplot(simDF,aes(x="Time",y="Population"))+geom_line()+geom_line(simDF,aes(x="Time",y="Population2"),color='red')+theme_classic()
 print(a3)
 
 # 4th simulation, carrying capacity = 2000, alpha = 0.0005
-params = (b, 0.0005, w, d, e, s)
+params = (b, a*0.4, w, d, e, s)
 sim = spint.odeint(func = RMA_Sim, y0 = y0, t = times, args = params)
 simDF = pandas.DataFrame({"Time":times,"Population":sim[:,0],"Population2":sim[:,1]})
 a4 = ggplot(simDF,aes(x="Time",y="Population"))+geom_line()+geom_line(simDF,aes(x="Time",y="Population2"),color='red')+theme_classic()
